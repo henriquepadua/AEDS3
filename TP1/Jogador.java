6cfffc65a85a8f5c8bc81f981d7cb2de98a2d6b2
henriquepadua <henrique.lommes@gmail.com>
2023-03-06 23:04:07 -0300

    Adicionando metodo Read

---
+++ TP1/Jogador.java
@@ -0,0 +1,71 @@
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class Jogador {
+    public int Id;
+    public int tamanhoRegistro;
+    public char lapide;
+    public String KnownAs;
+    public String FullName;
+    public int Overall;
+    public int Potential;
+    public int Value;
+    public String PositionsPlayed;
+    public String BestPosition;
+    public String Nationality;
+    public String ImageLink;
+
+    public Jogador(){
+        this.ImageLink = "";
+        this.BestPosition = "";
+        this.PositionsPlayed = "";
+        this.Value = 0;
+        this.Overall = 0;
+        this.FullName = "";
+        this.KnownAs = "";
+        this.Id = -1;
+        this.lapide = ' ';
+        this.tamanhoRegistro = 0;
+    }
+
+    public Jogador(int id,String KnownAs,String FullName,int Overall,int Potential,int Value,String PositionsPlayed,String BestPosition
+                ,String Nationality,String ImageLink,int tamanhoRegistro){
+        this.ImageLink = ImageLink;
+        this.BestPosition = BestPosition;
+        this.PositionsPlayed = PositionsPlayed;
+        this.Value = Value;
+        this.Overall = Overall;
+        this.FullName = FullName;
+        this.KnownAs = KnownAs;
+        this.Id = id;
+        this.tamanhoRegistro = tamanhoRegistro;
+    }
+
+    public byte[] toByteArray() throws IOException{
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            DataOutputStream dos = new DataOutputStream(baos);
+
+            dos.writeChar(lapide = ' ');           
+            dos.writeInt(Id);
+            dos.writeInt(KnownAs.length());
+            dos.writeUTF(KnownAs);           
+            dos.writeInt(FullName.length());
+            dos.writeUTF(FullName);
+            dos.writeInt(Overall);
+            dos.writeInt(Potential);
+            dos.writeInt(Value);
+            dos.writeInt(PositionsPlayed.length());
+            dos.writeUTF(PositionsPlayed);
+            dos.writeInt(BestPosition.length());
+            dos.writeUTF(BestPosition);
+            if(Nationality == null) Nationality = "";
+            dos.writeInt(Nationality.length());
+            dos.writeUTF(Nationality);
+            dos.writeInt(ImageLink.length());
+            dos.writeUTF(ImageLink);
+            
+            return baos.toByteArray();
+        }
+}
---
+++ TP1/Main.java
@@ -0,0 +1,316 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner sc = new Scanner(System.in);
+    public static int i = 0;
+
+    public static Jogador tratamentoFluxoDados(RandomAccessFile arquivodb,RandomAccessFile csv,Jogador fifa) throws IOException{
+        /* Declaraçaão de variaveis */
+        String line = csv.readLine();
+
+        String[] elemento = new String[100]; 
+        
+        int j = 0,tamanhoString = 0, controle = 0,controleValue = 0,controlePositionPlayer = 0,controleBestPosition = 0,controleNationality = 0,
+            controleImage = 0,controleFullName = 0,controleOverall = 0,controlePotential = 0;
+
+        //tratamento de dados da extensão csv    
+        while(line.charAt(j) != ','){//enquanto não achar uma virgula soma caractere por caractere
+            if(controle > 8) break;
+            elemento[i] += line.charAt(j);
+            j++;//somo mais um para
+            if(j == line.length()){ break;}//se a linha chegar ao fim sai do loop
+            if(line.charAt(j) == ','){
+                    elemento[i] = elemento[i].replaceAll("null", "");
+                    elemento[i] += ",";
+                    tamanhoString = elemento[i].length() - tamanhoString;
+                    if(controle == 0){ fifa.KnownAs = elemento[i]; controle++;controleFullName = j;}
+
+                    else if(controle == 1){ fifa.FullName = elemento[i].substring(controleFullName+1,j); controle++;controleOverall = j;}
+                    else if(controle == 2){ fifa.Overall =   Integer.parseInt(elemento[i].substring(controleOverall+1,j)); controle++;controlePotential = j;}
+                    else if(controle == 3){ fifa.Potential = Integer.parseInt(elemento[i].substring(controlePotential+1,j)); controle++; controleValue = j;}
+
+                    else if(controle == 4){ fifa.Value = Integer.parseInt(elemento[i].substring(controleValue + 1 ,j)); controle++; controlePositionPlayer = j;}
+                     
+                    else if(controle == 5){
+                        //System.out.println(line.charAt(controlePositionPlayer+1));   
+                        int aux = controlePositionPlayer+2;                                             
+                        
+                        if(line.charAt(controlePositionPlayer+1) == '"'){ 
+                            while(line.charAt(aux) != '"'){
+                                fifa.PositionsPlayed += line.charAt(aux);  
+                                aux++;
+                            }
+
+                            controle++; controleBestPosition = aux; 
+                        }
+                        else{
+                            fifa.PositionsPlayed = elemento[i].substring(j-2,j); 
+                            controle++; controleBestPosition = j;
+                        }
+                    }
+                    else if(controle == 6){ 
+                        int aux = controleBestPosition + 2;
+                        
+                        if(line.charAt(controleBestPosition+1) == ','){
+                            while(line.charAt(aux) != ','){
+                                fifa.BestPosition += line.charAt(aux);  
+                                aux++;
+                            }
+                            controle++; controleNationality = aux;
+                        }
+                        else{
+                            fifa.BestPosition = elemento[i].substring(controleBestPosition+1,j); controle++; controleNationality = j;
+                        }
+                    }    
+                    
+                    else if(controle == 7){ 
+                       // System.out.println(line.charAt(controleNationality));
+                        int aux = controleNationality + 1;
+                        
+                        if(line.charAt(controleNationality) == ','){
+                            while(line.charAt(aux) != ','){
+                                fifa.Nationality += line.charAt(aux);  
+                                fifa.Nationality = fifa.Nationality.replaceAll("null", "");
+                        
+                                aux++;
+                            }
+                            controle++; controleImage = aux;
+                        }
+                        else{
+                            fifa.Nationality = elemento[i].substring(controleNationality+1,j); controle++; controleImage = j;
+                        }
+                    }
+                    else if(controle == 8){ 
+                        //System.out.println(line.charAt(controleImage));
+                        int aux = controleImage + 1;
+
+                        if(line.charAt(controleImage) == ','){
+                            while(line.charAt(aux) != ','){
+                                fifa.ImageLink += line.charAt(aux);  
+                                fifa.ImageLink = fifa.ImageLink.replaceAll("null", "");
+
+                                aux++;
+                            }
+                            controle++;
+                        }
+
+                        else{
+                            fifa.ImageLink   = elemento[i].substring(controleImage+1,j); controle++; 
+                        }
+                    }
+                    j++;
+            }
+        }
+        i++;
+        return fifa;
+    }    
+
+    public static Jogador Create(RandomAccessFile arquivodb,RandomAccessFile csv,Jogador fifa) throws IOException{  
+        if(fifa != null){         
+            fifa = tratamentoFluxoDados(arquivodb, csv, fifa);
+            
+            arquivodb.seek(0);
+            int ultimoId = arquivodb.readInt();
+            
+            fifa.Id = ultimoId + 1;
+            arquivodb.seek(0);
+            
+            arquivodb.writeInt(fifa.Id);
+            
+            arquivodb.seek(arquivodb.length());
+            byte[] registro = fifa.toByteArray();
+            arquivodb.writeInt(registro.length);
+
+            arquivodb.write(registro);
+                
+            return fifa;
+        }
+        return null;
+    }
+
+    public static long buscaId (RandomAccessFile arq, long comeco, int idDesejada) { // retorna a posicao do registro com a id desejada no arquivo, antes do indicador de tamanho
+    	try {
+	    	arq.seek(comeco);
+			int ultimaId = arq.readInt();
+			int tamRegAtual;
+			long pos0;
+			int idAtual = 0;
+			if(ultimaId > 0 ) { // se tiver mais de um registro
+				do {
+                    char lapide = arq.readChar();
+					pos0 = arq.getFilePointer();
+					tamRegAtual = arq.readInt();
+					if(lapide != '*') { // se o registro atual nao tiver sido deletado
+						idAtual = arq.readInt();
+						
+						if(idAtual!=idDesejada) { // se nao for o registro desejado, pula para o proximo
+							arq.seek(pos0);
+							arq.readInt();
+							arq.skipBytes(tamRegAtual);
+						}
+						
+					} else { // se o registro atual tiver sido deletado
+						arq.seek(pos0); // vai para o proximo registro
+						arq.readInt();
+						arq.skipBytes(tamRegAtual);
+					}
+				} while (idAtual!=ultimaId && idAtual!=idDesejada); // repete para cada registro ate chegar no ultimo ou ate chegar na id desejada
+				
+				if(idAtual == idDesejada) { // se encontramos o registro desejado, retorna a posicao do registro no arquivo
+					return pos0;
+				} else { // se nao encontramos o registro, retorna -1
+					return -1;
+				}
+			} else { // se nao tiver nenhum registro
+				return -1;
+			}
+    	} catch(IOException e) {
+			System.out.println(e.getMessage());
+		}
+    	return -1;
+    }
+
+    public static String Read(RandomAccessFile arquivodb,int id){
+        System.out.println("\n=== BUSCAR UM JOGADOR ===\n");
+		System.out.println("Digite a ID do Jogador que quer exibir:");
+
+        int tamanhoString;
+        String KnownAs;
+        String FullName;
+        int Overall;
+        int Potential;
+        int Value;
+        String PositionsPlayed;
+        String BestPosition;
+        String Nationality;
+        String ImageLink;
+        int idAtual = 0;
+        long pos = 0;
+        
+        int idDesejada = sc.nextInt();
+		sc.nextLine();
+        
+        try {
+			arquivodb.seek(0);
+			int ultimaId = arquivodb.readInt();
+            
+			if(ultimaId > 0 ) { // se tiver mais de um registro
+                
+				do {
+					int tamRegAtual = arquivodb.readInt();
+					pos = arquivodb.getFilePointer();
+					if(arquivodb.readChar() != '*') { // se o registro atual nao tiver sido deletado
+ 						idAtual = arquivodb.readInt();
+						
+						if(idAtual!=idDesejada) { // se nao for o registro desejado, pula para o proximo
+                            arquivodb.seek(pos);
+							arquivodb.skipBytes(tamRegAtual);
+						}
+						
+					} else { // se o registro atual tiver sido deletado
+						arquivodb.seek(pos + tamRegAtual); // vai para o proximo registro
+					}
+				} while (idAtual!=ultimaId && idAtual!=idDesejada); // repete para cada registro ate chegar no ultimo ou ate chegar na id desejada
+				
+				if(idAtual == idDesejada) { // se encontramos o registro desejado, le os dados do registro e o imprime
+					tamanhoString = arquivodb.readInt();
+					KnownAs = arquivodb.readUTF();
+					tamanhoString = arquivodb.readInt();
+					FullName = arquivodb.readUTF();
+					Overall = arquivodb.readInt();
+                    Potential = arquivodb.readInt();
+                    Value = arquivodb.readInt();
+                    tamanhoString = arquivodb.readInt();
+					PositionsPlayed = arquivodb.readUTF();
+                    tamanhoString = arquivodb.readInt();
+					BestPosition = arquivodb.readUTF();
+                    tamanhoString = arquivodb.readInt();
+					Nationality = arquivodb.readUTF();
+					tamanhoString = arquivodb.readInt();
+					ImageLink = arquivodb.readUTF();
+					
+					Jogador jogador = new Jogador(idDesejada, KnownAs, FullName, Overall, Potential, Value, PositionsPlayed, BestPosition, Nationality, ImageLink, ultimaId);
+					System.out.println("\nJogador #" + idAtual + ":");
+					//System.out.println(jogador.toString());
+					System.out.println("\nAperte enter para continuar.");
+					sc.nextLine();
+					return "Deu bom";
+				} else {
+					System.out.println("\nJogador não encontrado. Aperte enter para continuar.");
+					sc.nextLine();
+					return null;
+				}
+			} else { // se nao tiver nenhum registro
+				System.out.println("\nBanco de dados vazio. Aperte enter para continuar.");
+				sc.nextLine();
+				return null;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+    public static void main(String[] args) throws IOException 
+    {
+        RandomAccessFile csv = new RandomAccessFile("Fifa 23 Players Data.csv", "rw");
+        RandomAccessFile arquivodb = new RandomAccessFile("canais.db", "rw");
+
+        arquivodb.seek(0); 
+        
+        if(arquivodb.length() == 0) arquivodb.writeInt(0);   arquivodb.seek(arquivodb.length());
+
+        csv.readLine();
+       // tratamentoDadosIniciais(csv, arquivodb);
+
+        boolean sair = false;
+        String opcao; Scanner sc = new Scanner(System.in);
+        Jogador fifa = new Jogador();
+        while(!sair) { // mostra o menu enquanto a pessoa nao escolher sair
+            System.out.println("=== SISTEMA DE CONTAS ===\n");
+            System.out.println("Escolha uma opção:");
+            System.out.println("1) Criar Jogador");
+            System.out.println("2) Pesquisar Jogador");
+            System.out.println("4) Alterar Jogador");
+            System.out.println("5) Deletar Jogador");
+            System.out.println("6) Intercalação balanceada comum");
+            System.out.println("S) Sair");
+            opcao = sc.nextLine();
+            switch(opcao) { // trata as opcoes
+                case "1":
+                fifa = new Jogador();
+                    fifa = Create(arquivodb,csv, fifa);
+                    break;
+                case "2":
+                    Read(arquivodb, 0);
+                    break;
+                // case "3":
+                //     transferir(arq, comeco);
+                //     break;
+                // case "4":
+                //     fifa = alterar(arq, comeco);
+                //     break;
+                // case "5":
+                //     deletar(arq, comeco);
+                //     break;
+                // case "6":
+                //     intercalacaoBalanceada2(arq, comeco);
+                //     break;
+                //     System.out.println("\nOpção inválida. Tente novamente.\n\n");
+                //     break;
+                case "s":
+                    sair = true;
+                    System.out.println("Saindo...");
+                    sc.close();
+                    break;
+                case "S":
+                    sair = true;
+                    System.out.println("Saindo...");
+                    sc.close();
+                    break;
+            }
+        }
+    }
+}
